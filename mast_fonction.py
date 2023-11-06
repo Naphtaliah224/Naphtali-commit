c133d1e06b71e66e083f78738721a7be306ca10e
Naphtaliah224 <96439684+Naphtaliah224@users.noreply.github.com>
2023-11-06 17:27:57 -0500

    wait

---
+++ mast_fonction.py
@@ -0,0 +1,94 @@
+import   random
+import re
+import string
+from slugify import slugify
+from random import choice
+
+#1 inves yon mo
+def tounen_inves(mot):
+    return mot[::-1]
+mo_inves = tounen_inves("Naphtala")
+print(mo_inves)
+
+#2
+def kod(n):
+    me = string.ascii_letters
+    ma = ''.join(random.choice(me)for _ in range(n))
+    return ma
+
+kod_aleyatwa=kod(10)
+print(kod_aleyatwa)
+
+# 3.-kod  aleyatwa alfabetik san repetisyon
+alfa1="abcdefghijklmnopqrstuvwxyz"
+aleyatwa1 = lambda alfa,nomb : "".join(random.sample(alfa,nomb))
+
+
+kod1=aleyatwa1(alfa1,10)
+print(kod1)
+print(len(kod1))
+
+
+# 4.-kod aleyatwa alfanimerik san repetisyon
+
+alfa2="abcdefghijklmnopqrstuvwxyz1234567890"
+
+aleyatwa2=lambda alfa,nomb: "".join(random.sample(alfa,nomb))
+
+
+kod2=aleyatwa2(alfa2,36)
+print(kod2)
+print(len(kod2))
+
+# 5.-jenere yon SLUG 
+
+alfa3="yon --c'henn pou fé\\slug"
+
+slug= lambda alfa: slugify(alfa)
+
+print(slug(alfa3))
+
+# 6.- separe chak let pa yon vigil
+mesaj1="metevigigilyoaprelet"
+
+met_vigil=lambda mesaj: ",".join([elt for elt in mesaj])
+
+print(met_vigil(mesaj1))
+
+# 7.- kripte yon mo ak vale index li nan alfabe a
+
+mesaj2= "ALO"
+
+crypt=lambda mesaj: "-".join([str(ord(elt)-97) for elt in mesaj.lower()])
+
+print(crypt(mesaj2))
+
+# 8.- dekripte yon kod avek valel nan alfabe a
+
+mesaj3="0-11-14"
+
+decrypt=lambda mesaj: "".join(chr(int(elt)+97) for elt in re.findall(r'\d+',mesaj))
+
+print(decrypt(mesaj3))
+
+# 9.- pemite de(2)varyab
+
+pemite1="Bonjou"
+pemite2="Klas"
+
+def permute(var1,var2):
+    var1,var2=var2,var1
+    tup = (var1,var2)
+    return tup
+    # print(f"Pemite1 = {var1}\nPemite2 = {var2}")
+
+print(permute(pemite1,pemite2))
+
+
+# 10.-FOnksyon pou inisyal
+
+nom="JEAN-BAPTISTE JEAN"
+
+inisyal=lambda nom: "".join([n[0] for n in nom.replace("-"," ").split()])
+
+print(inisyal(nom)) 
